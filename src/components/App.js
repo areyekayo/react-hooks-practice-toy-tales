53777c4aaff31fda955a99668d8be9aa2b6d62ae
Riko <riko.fluchel@gmail.com>
2025-03-29 10:20:34 -0400

    add new toy form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
@@ -18,10 +17,15 @@ function App() {
       .then((data) => setToys(data))
   })
 
+  function addNewToy(newToy){
+    console.log("new toy", newToy)
+    setToys([...toys, newToy])
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onSubmitToy={addNewToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
